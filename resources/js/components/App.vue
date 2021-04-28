52cfdd51a0c7d459ce107701e798ecf8253965ad
Jeroen Postema <jeroen@script.nl>
2021-04-28 11:25:52 +0200

    add project files

---
+++ resources/js/components/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+        <form action="/login" method="post">
+            <input type="hidden" name="_token" :value="csrf">
+            username: <input type="text" name="email"><br>
+            password: <input type="text" name="password"><br>
+            <input type="submit" value="inloggen">
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() 
+        {
+            return {
+                csrf: document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+            }
+        },
+        mounted() {
+            axios.get("/api/users").then(function (resp) {
+                console.log(resp);
+            })["catch"](function (errors) {
+                console.log(errors);
+            });  
+        },
+    }
+</script>
+
+
